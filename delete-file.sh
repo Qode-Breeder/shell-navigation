3db12219f887c9d4e9d2bd50ee4b9c910ade2b26
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-21 01:18:50 -0500

    Create a file then delete it

---
+++ delete-file.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Write a script that creates a file in the `/root` dir
+# then deletes it
+
+cd /
+pwd
+touch ready_to_be_removed.sh # Creates file
+rm ready_to_be_removed.sh # Deletes file
