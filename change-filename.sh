f6453d10252e650319acc33872f0c6f51731b10c
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-21 00:17:05 -0500

    Create a new file then update its name

---
+++ change-filename.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Write a script that creates a new file `old_school.sh` in so_cool/
+# then update the file name to `new_school.sh`.
+
+pwd # Check current working dir
+touch old_school.sh # Create new file
+mv old_school new_school.sh # Rename the file
