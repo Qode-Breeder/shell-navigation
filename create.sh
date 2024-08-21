c679e33196e64c062ad6233ae2f3570b88f1c692
Frandy Slueue <Frandy4ever@gmail.com>
2024-08-21 00:00:58 -0500

    Update from create to create.sh

--- create.sh
+++ create.sh
@@ -5,5 +5,5 @@
 pwd # Check working directory.
 cd / # Change to `/root` directory.
 pwd # Verify `/root` directory.
-mkdir so_cool && cd so_cool  # Create `so_cool/` and change to it.
+mkdir so_cool && cd so_cool  # Create `so_cool/` dir and change to it.
 pwd # Check that working directory is `so_cool/`.
